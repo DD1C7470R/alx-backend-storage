0c6ef76a627d663b5a94c8196b3aacd125c11de3
DD1C7470R <gracevalley101@gmail.com>
2024-01-18 22:52:00 +0000

    Change implmentation

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -6,7 +6,9 @@ CREATE TRIGGER reset_email BEFORE UPDATE ON `users`
 FOR EACH ROW
 BEGIN
     IF NEW.email <> OLD.email THEN
-        SET NEW.valid_email = (SELECT CASE WHEN OLD.valid_email = 1 THEN 0 ELSE 1 END);
+        SET NEW.valid_email = 0;
+	 ELSE 
+	SET NEW.valid_email = NEW.valid_email;
     END IF;
 END;
 $$
