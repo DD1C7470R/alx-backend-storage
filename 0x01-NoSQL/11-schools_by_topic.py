2203c53e6264ced19f2f6f5bdc50b3b6cc8c2635
DD1C7470R <gracevalley101@gmail.com>
2024-01-23 01:58:25 -0800

    Hot fix task 11

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+""" 11-main """
+
+def schools_by_topic(mongo_collection,  topic):
+    """changes all topics of a school document based on the name"""
+    return [doc for doc in mongo_collection.find({"topic": topic})]
